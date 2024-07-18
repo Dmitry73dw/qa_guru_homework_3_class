e648035ad053d1a78f55ee9dfdecb456bb948af8
Dmitry Unitcaev <Dima.unitzaev@icloud.com>
2024-07-19 00:16:59 +0400

    adjustment test_search_without_result

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,15 +1,21 @@
 import pytest
 from selene import browser, be, have
-import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def open_brawser():
     browser.open('https://google.com')
+    yield
+    browser.close()
+
+@pytest.fixture
+def open_main_page():
+    browser.element('[class="jfN4p"]').click()
+
 
 def test_modification_google_search(open_brawser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_search_without_result(open_brawser):
+def test_search_without_result(open_main_page):
     browser.element('[name="q"]').should(be.blank).type('adgasefASCAWERWERew').press_enter()
     browser.element('[class="card-section"]').should(have.text('ничего не найдено'))
