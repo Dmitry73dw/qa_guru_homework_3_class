07d717caf3f19f9efe150d905ee457ab6c618506
Dmitry Unitcaev <Dima.unitzaev@icloud.com>
2024-07-18 23:54:26 +0400

    add test_search_without_result

--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -9,3 +9,7 @@ def open_brawser():
 def test_modification_google_search(open_brawser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_search_without_result(open_brawser):
+    browser.element('[name="q"]').should(be.blank).type('adgasefASCAWERWERew').press_enter()
+    browser.element('[class="card-section"]').should(have.text('ничего не найдено'))
